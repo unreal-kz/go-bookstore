8485d8804b057043c9155df71fa07d75e65e05da
unreal-kz <ukibas.b@gmail.com>
2022-06-24 11:51:06 +0300

    book-controller.go and bookstore-routes.go have been changed

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -13,8 +13,9 @@ import (
 
 var NewBook models.Book
 
-func GetBook(w http.ResponseWriter, r *http.Request) {
+func GetBooks(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
+
 	res, _ := json.Marshal(newBooks)
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
